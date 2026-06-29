46626558df82cf57ca86e8ef8cdc59b4ec6a30ee
athayanajwan <athaya123hafidz@gmail.com>
2026-06-30 06:29:25 +0700

    cors

--- lms-smart-backend/nest-backend/src/main.ts
+++ lms-smart-backend/nest-backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       "http://localhost:3001",
-      "https://lms-smart-j05pdxuz6-athayahapidzs-projects.vercel.app",
+      "https://lms-smart.vercel.app",
     ],
     credentials: true,
   });
